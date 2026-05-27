f24312d8a8b3b411686cd5dd88b0c7adb4294709
CDhivyaBharathi <bharathi.chellakumar@gmail.com>
2026-05-28 01:06:04 +0530

    feat:refactor using structs and added debugging derived trait

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
-fn main() {
-    let width1 = 30;
-    let height1 = 50;
+// fn main() {
+//     let rect1 = (30 , 50);
 
-    println!("The area is {}",area(width1,height1));
+//     println!("The area is {}",area(rect1));
+// }
+
+
+// fn area(dim: (u32, u32)) -> u32{
+//     dim.0 * dim.1
+// }
+
+//refactoring with structs
+
+#[derive(Debug)] //enables debug format
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
+fn main(){
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30*scale), // dbg! returns ownership of the expression’s value
+        height: 50,
+    };
 
-fn area(width: u32, height:u32) -> u32{
-    width*height
+    println!("The rect is {rect1:#?}"); //:? or :#? tells the macro to print in 'debug' format
+    println!("The area is {}",areaFind(&rect1));
+
+    dbg!(&rect1);
+
+}
+
+fn areaFind(rect1: &Rectangle) -> u32 { 
+//borrowed immutable referrence in the signature cuz we don't want to 
+//away the ownership from the main function.
+    rect1.width * rect1.height
 }
